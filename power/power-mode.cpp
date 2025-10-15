52ea90e4b04df378948f39668efd5b12ca37510a
rio004 <riyon336@gmail.com>
2025-10-15 12:13:05 +0100

    sm8850-common: power: Allow fod to be enabled when display is off
    Change-Id: Ib63491df87cb3c2e7c0e8e8848ba6fc7d9ea1c27
    Signed-off-by: rio004 <riyon336@gmail.com>

--- power/power-mode.cpp
+++ power/power-mode.cpp
@@ -14,6 +14,7 @@
 #define SELECT_TOUCH_ID 3
 #define SET_CUR_VALUE 0
 #define TOUCH_DOUBLETAP_MODE 14
+#define TOUCH_FOD_ENABLE_MODE 10
 #define TOUCH_MAGIC 0x54
 #define TOUCH_DEV_PATH "/dev/xiaomi-touch"
 #define TOUCH_ID 0
@@ -40,6 +41,7 @@ using ::aidl::android::hardware::power::Mode;
 bool isDeviceSpecificModeSupported(Mode type, bool* _aidl_return) {
     switch (type) {
         case Mode::DOUBLE_TAP_TO_WAKE:
+        case Mode::DISPLAY_INACTIVE:
             *_aidl_return = true;
             return true;
         default:
@@ -56,6 +58,19 @@ bool setDeviceSpecificMode(Mode type, bool enabled) {
             data.touch_id = TOUCH_ID;
             data.cmd = SET_CUR_VALUE;
             data.mode = TOUCH_DOUBLETAP_MODE;
+            data.data_len = 1;
+            data.data_buf[0] = enabled ? 1 : 0;
+            ioctl(fd, TOUCH_IOC_COMMON_DATA, &data);
+            close(fd);
+            return true;
+        }
+        case Mode::DISPLAY_INACTIVE: {
+            int fd = open(TOUCH_DEV_PATH, O_RDWR);
+            ioctl(fd, TOUCH_IOC_SELECT_TOUCH_ID, TOUCH_ID);
+            touch_data data = {};
+            data.touch_id = TOUCH_ID;
+            data.cmd = SET_CUR_VALUE;
+            data.mode = TOUCH_FOD_ENABLE_MODE;
             data.data_len = 1;
             data.data_buf[0] = enabled ? 1 : 0;
             ioctl(fd, TOUCH_IOC_COMMON_DATA, &data);
